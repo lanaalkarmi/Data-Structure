9f75ceb4d843ecf796f25dfae2a5d2e0e44b9ed7
lana alkarmi <lanakarmi05@gmail.com>
2026-07-04 13:48:48 +0300

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+// unordered set
+
+
+
+
+
+
+int main()
+{
+	unordered_set<int>s;
+	s.insert(3);
+	if (!s.empty()) {
+		cout << " not empty";
+	}
+	//1
+	if (s.count(3) > 0) {
+		cout << " found";
+	}
+	//2
+	auto it = s.find(3);// find هي عبارة عن iterator
+
+	if (it != s.end()) {
+		cout << " found";
+
+	}
+	for (auto x : s) {
+		cout << x;
+		// لانو الماب هي عبارة عن قيمة وحدة بينما السيت عبارة عن pair 
+	}
+}
+
